bc133118393ffacdbd5fa7f154dc33cdfa303451
Dilsher <dilsherb@gmail.com>
2023-07-25 13:33:05 +0300

    animal class altered

--- animal.rb
+++ animal.rb
@@ -1,6 +1,16 @@
 require_relative 'remover'
 
 class Animal
+  def initialize(number_of_legs, name = "Unknown")
+    @id = Random.rand(1..1000)
+    @name = name
+    @number_of_legs = number_of_legs
+  end
+
+  def speak
+    "Bla bla bla"
+  end
+  
   def remove_leg
     remover = Remover.new()
     @number_of_legs = remover.decrease(@number_of_legs)
