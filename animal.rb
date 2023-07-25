2a34fbfd872cdceb30a7d4017f386f37926fcffa
Dilsher <dilsherb@gmail.com>
2023-07-25 13:44:05 +0300

    animal class updated.

--- animal.rb
+++ animal.rb
@@ -1,16 +1,40 @@
 require_relative 'remover'
 
 class Animal
-  def initialize(number_of_legs, name = "Unknown")
+  attr_reader :id, :type, :number_of_legs
+  attr_accessor :name
+
+  def initialize(type, number_of_legs, name = "Unknown")
     @id = Random.rand(1..1000)
     @name = name
     @number_of_legs = number_of_legs
+    @type = type
   end
 
   def speak
-    "Bla bla bla"
+    if @type == "dog"
+      "Woof!, Woof!"
+    elsif @type == "spider"
+      "Sssssssssss"
+    elsif @type == "cat"
+      "Meow!"
+    end
+  end
+
+  def bring_a_stick
+    if @type == "dog"
+      "Here is your stick!"
+    elsif @type == "spider"
+      "www"
+    elsif @type == "cat"
+      "I'm not a dog!"
+    end
   end
   
+
+    "Bla bla bla"
+  end
+
   def remove_leg
     remover = Remover.new()
     @number_of_legs = remover.decrease(@number_of_legs)
