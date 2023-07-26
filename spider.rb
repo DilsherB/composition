b85df91f53794b74c2e8c1027c4c98df214872cf
Dilsher Balouch <dilsherb@gmail.com>
2023-07-26 18:06:11 +0300

    liked_food variable having value of spiderFood method from animal

--- spider.rb
+++ spider.rb
@@ -4,6 +4,7 @@ class Spider < Animal
   def initialize(web_strength_level, name = 'unknown')
     super('spider', 8, name)
     @web_strength_level = web_strength_level
+    @liked_food = SpiderFood.new()
   end
 
   def make_a_web
